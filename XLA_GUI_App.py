3d22a51fb3d328f39441f4fca22d3e784bf61996
Your Name <you@example.com>
2021-04-06 02:05:49 +0700

    add button Binary

--- XLA_GUI_App.py
+++ XLA_GUI_App.py
@@ -7,6 +7,7 @@ from os import path
 import numpy as np
 import scipy.misc
 from scipy import ndimage
+import matplotlib.pyplot as plt
 
 root = Tk()
 root.title("Xử lý ảnh")
@@ -219,7 +220,64 @@ def Gray_Scale(input_image):
     return
 
 
-def Histogram():
+def Histogram(input_image):
+    im = cv.imread(input_image)
+    # calculate mean value from RGB channels and flatten to 1D array
+    vals = im.mean(axis=2).flatten()
+    # plot histogram with 255 bins
+    b, bins, patches = plt.hist(vals, 255)
+    plt.xlim([0, 255])
+    plt.show()
+    return
+
+
+def Binary(input_image):
+    hist = []
+    for i in range(0, 377):
+        hist.append(0)
+    binary_pt_image = Image.open(input_image)
+    pixel_val = binary_pt_image.load()
+
+    for i in range(binary_pt_image.size[0]):
+        for j in range(binary_pt_image.size[1]):
+            sum = 0
+            for k in range(0, 3):
+                sum += pixel_val[i, j][k]
+            sum = sum//3
+            # print(sum)
+            hist[sum] += 1
+    percentage = 30
+    threshold_pixels = binary_pt_image.size[0]*binary_pt_image.size[1]*0.6
+    print(binary_pt_image.size[0]*binary_pt_image.size[1])
+    print(threshold_pixels)
+
+    for i in range(376, -1, -1):
+
+        sum += hist[i]
+        if(sum > threshold_pixels):
+            threshold_value = i
+            break
+    print(threshold_value)
+    for i in range(binary_pt_image.size[0]):
+        for j in range(binary_pt_image.size[1]):
+            sum = 0
+            for k in range(0, 3):
+                sum += pixel_val[i, j][k]
+            sum = sum//3
+            if(sum > threshold_value):
+                pixel_val[i, j] = (255, 255, 255)
+            else:
+                pixel_val[i, j] = (0, 0, 0)
+
+    img_path = "BinaryPT.jpg"
+    binary_pt_image.save(img_path)
+    # resize Image
+    resize_bf = Image.open(img_path).resize((600, 450), Image.ANTIALIAS)
+    # # convert images to ImageTK format
+    img = ImageTk.PhotoImage(resize_bf)
+    # set image to Label
+    box_img_after.configure(image=img)
+    box_img_after.image = img
     return
 
 
@@ -260,7 +318,12 @@ btn_Gray_Scale.grid(column=2, row=3)
 
 # button Histogram
 btn_Histogram = Button(root, text="Histogram", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=Histogram)
+    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=lambda: Histogram(temp_img))
 btn_Histogram.grid(column=3, row=3)
+
+# button Sobel
+btn_Binary = Button(root, text="Binary", font=(
+    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=lambda: Binary(temp_img))
+btn_Binary.grid(column=0, row=5)
 
 root.mainloop()
