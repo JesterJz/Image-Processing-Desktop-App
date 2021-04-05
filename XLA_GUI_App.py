a4fca27987f5065b68d27212ddee414072ea9898
JesterJz <zimmer.jeyy@gmail.com>
2021-04-05 15:44:16 +0700

    GUI Last commit

--- XLA_GUI_App.py
+++ XLA_GUI_App.py
@@ -2,8 +2,8 @@ from tkinter import *
 from PIL import Image, ImageTk
 root = Tk()
 root.title("Xử lý ảnh")
-root.geometry("630x900")
-# root.maxsize(800, 800)
+root.geometry("600x880")
+root.maxsize(600, 880)
 # root.iconphoto('bg.jpg')
 
 # load = Image.open('./Image/bg.jpg')
@@ -13,15 +13,16 @@ root.geometry("630x900")
 
 root.configure(background='pink')
 
-name = Label(root, text="Xử lý ảnh", fg="#000", bd=0, bg="pink")
+name = Label(root, text="Jester Jz", fg="#000", bd=0, bg="pink")
 name.config(font=("Arial", 20))
 name.pack(pady=10)
 
 # title Image before
 before_name = Label(root, text="Image Before", fg="#000", bd=0, bg="pink")
-before_name.config(font=("Arial", 18))
+before_name.config(font=("Arial", 16))
 before_name.pack()
 
+# Image box before
 # Open Img
 pic_bf = Image.open('./Image/bg.jpg')
 # resize Img
@@ -33,9 +34,10 @@ box_img_before.pack(pady=10)
 
 # title Image After
 after_name = Label(root, text="Image After", fg="#000", bd=0, bg="pink")
-after_name.config(font=("Arial", 18))
+after_name.config(font=("Arial", 16))
 after_name.pack()
 
+# Image box before
 # Open Img
 pic_af = Image.open('./Image/a.jpg')
 # resize Img
@@ -43,9 +45,9 @@ resize_af = pic_af.resize((450, 340), Image.ANTIALIAS)
 
 img_af = ImageTk.PhotoImage(resize_af)
 box_img_after = Label(root, image=img_af)
-box_img_after.pack(pady=20)
+box_img_after.pack(pady=10)
 
-frame = Frame(root).pack(side=BOTTOM)
+frame = Frame(root).pack(pady=10)
 
 
 def clear():
@@ -56,13 +58,26 @@ def select():
     return
 
 
+def open_camera():
+    return
+
+
+# button select
 btn_select = Button(frame, text="Select Image", font=(
     ("Arial"), 10, 'bold'), bg='#303030', fg='#fff', command=select())
-btn_select.place(x=150, y=850)
-
-
+btn_select.place(x=100, y=840)
+# button open camera
+btn_open_cam = Button(frame, text="Open Cam", font=(
+    ("Arial"), 10, 'bold'), bg='#303030', fg='#fff', command=open_camera())
+btn_open_cam.place(x=200, y=840)
+# button clear
 btn_clear = Button(frame, text="Clear Image", font=(
     ("Arial"), 10, 'bold'), bg='#303030', fg='#fff', command=clear())
-btn_clear.place(x=400, y=850)
+btn_clear.place(x=355, y=840)
+# button quit
+btn_cls = Button(frame, text="Quit", font=(
+    ("Arial"), 10, 'bold'), bg='#303030', fg='#fff', command=root.quit)
+btn_cls.place(x=450, y=840)
+
 
 root.mainloop()
