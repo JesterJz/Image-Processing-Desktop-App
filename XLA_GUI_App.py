bbcd4a90a3ae31f1d0521ad848a26e483c0298fc
pmh142857 <thehunter153180@gmail.com>
2021-04-08 00:33:22 +0700

    Change the interface and add button click notifications v.1

--- XLA_GUI_App.py
+++ XLA_GUI_App.py
@@ -8,10 +8,13 @@ import numpy as np
 import scipy.misc
 from scipy import ndimage as nd
 import matplotlib.pyplot as plt
+from tkinter import messagebox
 
 root = Tk()
-root.title("Xử lý ảnh")
-root.geometry("1920x1080")
+root.title("Image processing")
+root.geometry("1920x1080+0+0")
+root.wm_resizable(width = True, height= True)
+root.configure(background='#80CBC4')
 # root.maxsize(1920, 1080)
 
 img_counter = 0
@@ -19,14 +22,14 @@ temp_img = None
 # path = None
 
 # Set Title Name App
-name = Label(root, text="Jester Jz", fg="#000", bd=0, bg="pink")
+name = Label(root, text="Image processing analysis: v1.0", fg="#000", bd=0, bg="#80CBC4")
 name.config(font=("Engravers MT", 20))
-name.grid(column=0, row=0, columnspan=6, pady=10)
+name.grid(column=0, row=0, columnspan=2, pady=10)
 
 # title Image before
-before_name = Label(root, text="Image Before", fg="#000", bd=0, bg="pink")
-before_name.config(font=("Arial", 16, "bold"))
-before_name.grid(column=0, row=1, columnspan=3)
+before_name = Label(root, text="Image Before:", fg="#FFFFFF", bd=0, bg="#80CBC4")
+before_name.config(font=("Arial", 20, "bold"))
+before_name.grid(column=0, row=1, columnspan=2)
 
 # Image box before
 # Open Img
@@ -36,21 +39,30 @@ pic_default = Image.open('./Image/icon_default.png')
 img_def = ImageTk.PhotoImage(pic_default)
 
 box_img_before = Label(root, image=img_def, width=720,
-                       height=576, bg="#303030")
+                       height=576, bg="#00695C")
 box_img_before.grid(column=0, row=2, columnspan=3, padx=20, pady=5)
 
 # title Image After
-after_name = Label(root, text="Image After", fg="#000", bd=0, bg="pink")
-after_name.config(font=("Arial", 16, "bold"))
-after_name.grid(column=3, row=1, columnspan=3, padx=10, pady=5)
+after_name = Label(root, text="Image After: ", fg="#FFFFFF", bd=0, bg="#80CBC4")
+after_name.config(font=("Arial", 20, "bold"))
+after_name.grid(column=3, row=1, columnspan=2, padx=10, pady=5)
+
+# title Action
+after_name1 = Label(root, text="None", fg="#004D40", bd=1, bg="#66BB6A")
+after_name1.config(font=("Arial", 20, "bold"))
+after_name1.grid(column=4, row=1, columnspan=3, padx=10, pady=5)
+# Show Action
+
+
 
 # Image box before
 box_img_after = Label(root, image=img_def, width=720,
-                      height=576, bg="#303030")
+                      height=576, bg="#00695C")
 box_img_after.grid(column=3, row=2, columnspan=3, padx=20, pady=5)
 
 
 def clear():
+    after_name1.configure(text ="Clear")
     pic_default = Image.open('./Image/icon_default.png')
     # convert images to ImageTK format
     img_def = ImageTk.PhotoImage(pic_default)
@@ -63,16 +75,20 @@ def clear():
     box_img_after.image = img_def
     return 0
 
+def save_as():
+    messagebox.askquestion('Warning !!!', 'Sorry the feature is updating :((')
+
+    return 0
 
 def select():
     global temp_img
-
     path = filedialog.askopenfilename()
 
     if len(path) > 0:
         # load the image from disk
         img = cv.imread(path)
         temp_img = path
+
         # Convert img to RGB
         img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
@@ -89,7 +105,6 @@ def select():
         box_img_before.configure(image=img_bf)
         box_img_before.image = img_bf
     return 0
-
 
 def open_camera():
     global temp_img
@@ -110,7 +125,6 @@ def open_camera():
 
             # load the image from disk
             img_pic = cv.imread("./Image/take_of_pic.png")
-
             temp_img = "./Image/take_of_pic.png"
 
             # Convert img to RGB
@@ -137,29 +151,30 @@ def open_camera():
     return 0
 
 
-def Sobel(Image_path):
-    # im = cv.imread('./Image/a.jpg')
-    # im = im.astype('int32')
-    # dx = ndimage.sobel(im, 0)  # horizontal derivative
-    # dy = ndimage.sobel(im, 1)  # vertical derivative
-    # mag = np.hypot(dx, dy)  # magnitude
-    # mag *= 255.0 / np.max(mag)  # normalize (Q&D)
-    # mag.save('sobel.jpg')
+# def Sobel(Image_path):
+#     im = cv.imread('./Image/a.jpg')
+#     im = im.astype('int32')
+#     dx = ndimage.sobel(im, 0)  # horizontal derivative
+#     dy = ndimage.sobel(im, 1)  # vertical derivative
+#     mag = np.hypot(dx, dy)  # magnitude
+#     mag *= 255.0 / np.max(mag)  # normalize (Q&D)
+#     mag.save('sobel.jpg')
 
-    # # # Edge = np.sqrt(Edge_x**2 + Edge_y**2)
-    # # print(Edge_x)
-    # # # resize Image
-    # # resize_bf = Edge_x.resize((600, 450), Image.ANTIALIAS)
+#     # # Edge = np.sqrt(Edge_x**2 + Edge_y**2)
+#     # print(Edge_x)
+#     # # resize Image
+#     # resize_bf = Edge_x.resize((600, 450), Image.ANTIALIAS)
 
-    # convert images to ImageTK format
-    # img = ImageTk.PhotoImage(Image.open(Image_path))
-    # # set image to Label
-    # box_img_after.configure(image=img)
-    # box_img_after.image = img
-    return
+#     convert images to ImageTK format
+#     img = ImageTk.PhotoImage(Image.open(Image_path))
+#     # set image to Label
+#     box_img_after.configure(image=img)
+#     box_img_after.image = img
+#     return
 
 
 def Laplacian(input_image):
+    after_name1.configure(text = "Laplacian" )
     im = cv.imread(input_image, 0)
     temp = im.copy()
     # print(im.shape[0],im.shape[1])
@@ -181,6 +196,7 @@ def Laplacian(input_image):
 
 
 def Gray_Scale(input_image):
+    after_name1.configure(text ="Gray Scale")
     gray_img = Image.open(input_image)
     pixel_val = gray_img.load()
     print(pixel_val[0, 0])
@@ -197,6 +213,7 @@ def Gray_Scale(input_image):
 
 
 def Histogram(input_image):
+    after_name1.configure(text ="Histogram")
     im = cv.imread(input_image)
     # calculate mean value from RGB channels and flatten to 1D array
     vals = im.mean(axis=2).flatten()
@@ -208,11 +225,12 @@ def Histogram(input_image):
 
 
 def Binary(input_image):
+    after_name1.configure(text ="Binary")
     hist = []
     for i in range(0, 377):
         hist.append(0)
-    binary_pt_image = Image.open(input_image)
-    pixel_val = binary_pt_image.load()
+        binary_pt_image = Image.open(input_image)
+        pixel_val = binary_pt_image.load()
 
     for i in range(binary_pt_image.size[0]):
         for j in range(binary_pt_image.size[1]):
@@ -258,13 +276,11 @@ def Binary(input_image):
 
 
 def Image_Thresholding(input_image):
-
+    after_name1.configure(text ="Image Thresholding")
     # read image
     img = cv.imread(input_image)
-
     # convert grayscale image
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
     # Image_Thresholding
     blur = cv.GaussianBlur(gray, (1, 1), 0)
     ret, thers = cv.threshold(
@@ -279,10 +295,9 @@ def Image_Thresholding(input_image):
 
 
 def Reverse_Image(input_image):
-
+    after_name1.configure(text ="Reverse Image")
     # read image
     img = cv.imread(input_image)
-
     # convert grayscale image
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
@@ -295,7 +310,7 @@ def Reverse_Image(input_image):
 
 
 def Log_transf(input_image):
-
+    after_name1.configure(text ="Logarithmic Transforms")
     # read image
     img = cv.imread(input_image)
 
@@ -313,10 +328,9 @@ def Log_transf(input_image):
 
 
 def GLS(input_image):
-
+    after_name1.configure(text ="Grey Level Slicing")
     img = cv.imread(input_image, 0)
 
-    # # # convert grayscale image
     # gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     m, n = img.shape
@@ -328,7 +342,6 @@ def GLS(input_image):
 
     # create a array of zeros
     img_thresh_back = np.zeros((m, n), dtype=int)
-
     for i in range(m):
 
         for j in range(n):
@@ -345,16 +358,16 @@ def GLS(input_image):
 
 
 def Smoothing_Image(input_image):
-
+    after_name1.configure(text ="Smoothing Image")
     img = cv.imread(input_image)
-    blur = cv.blur(img, (20, 20))
+    blur = cv.blur(img, (5, 5))
     name_image = "Smoothing_Image_"
     save_show_image(name_image, blur)
     return 0
 
 
 def laplace_and_gau(input_image):
-
+    after_name1.configure(text ="Laplace and Gaussiane")
     img = cv.imread(input_image)
     LoG = nd.gaussian_laplace(img, 1)
     name_image = "laplace_and_gau_"
@@ -382,80 +395,86 @@ def save_show_image(name_image, image):
         box_img_after.image = img
     return
 
-
-# button Sobel
-btn_sobel = Button(root, text="Sobel", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=lambda: Sobel(temp_img))
-btn_sobel.grid(column=0, row=3, pady=5)
-
-# button Laplacian
-btn_Laplacian = Button(root, text="Laplacian", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=lambda: Laplacian(temp_img))
-btn_Laplacian.grid(column=1, row=3, pady=5)
-
-# button Gray Scale
-btn_Gray_Scale = Button(root, text="Gray Scale", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=lambda: Gray_Scale(temp_img))
-btn_Gray_Scale.grid(column=2, row=3, pady=5)
-
-# button Histogram
-btn_Histogram = Button(root, text="Histogram", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=lambda: Histogram(temp_img))
-btn_Histogram.grid(column=3, row=3, pady=5)
-
-# button Binary
-btn_Binary = Button(root, text="Binary", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=lambda: Binary(temp_img))
-btn_Binary.grid(column=4, row=3, pady=5)
-
-# button Reverse Image
-btn_Reverse_Image = Button(root, text="Reverse Image", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=lambda: Reverse_Image(temp_img))
-btn_Reverse_Image.grid(column=4, row=3, pady=5)
-
-# button Image Thresholding
-btn_Image_Thresholding = Button(root, text="Image Thresholding", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=lambda: Image_Thresholding(temp_img))
-btn_Image_Thresholding.grid(column=5, row=3, pady=5)
-
-# button Logarithmic transforms
-btn_Log_transf = Button(root, text="Logarithmic transforms", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=lambda: Log_transf(temp_img))
-btn_Log_transf.grid(column=0, row=4, pady=5)
-
-# button Grey level Slicing
-btn_GLS = Button(root, text="Grey level Slicing", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=lambda: GLS(temp_img))
-btn_GLS.grid(column=1, row=4, pady=5)
-
-# button Smoothing Image
-btn_Smoothing_Image = Button(root, text="Smoothing Image", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=lambda: Smoothing_Image(temp_img))
-btn_Smoothing_Image.grid(column=2, row=4, pady=5)
-
-# button Laplace and gaussian
-btn_laplace_and_gau = Button(root, text="Laplace and gaussian", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=lambda: laplace_and_gau(temp_img))
-btn_laplace_and_gau.grid(column=3, row=4, pady=5)
-
 # button select
 btn_select = Button(root, text="Select Image", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=select)
-btn_select.grid(column=4, row=4, pady=5)
+    ("Arial"), 10, 'bold'), bg='#00897B',width=12,height=1, fg='#FFFFFF', command=select)
+btn_select.grid(column=0, row=3, pady=5)
 
 # button open camera
 btn_open_cam = Button(root, text="Open Cam", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=open_camera)
-btn_open_cam.grid(column=5, row=4, pady=5)
+    ("Arial"), 10, 'bold'), bg='#00897B',width=12,height=1, fg='#FFFFFF', command=open_camera)
+btn_open_cam.grid(column=0, row=4, pady=5)
 
 # button clear
 btn_clear = Button(root, text="Clear Image", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=clear)
-btn_clear.grid(column=2, row=5, pady=5)
+    ("Arial"), 10, 'bold'), bg='#827717',width=12,height=1, fg='#FFFFFF', command=clear)
+btn_clear.grid(column=1, row=3, pady=5)
+
+# button Save as
+btn_save_as = Button(root, text="Save as", font=(
+    ("Arial"), 10, 'bold'), bg='#827717',width=12,height=1, fg='#FFFFFF', command=save_as)
+btn_save_as.grid(column=1, row=4, pady=5)
 
 # button quit
 btn_cls = Button(root, text="Quit", font=(
-    ("Arial"), 10, 'bold'), bg='#fff', fg='#000', command=root.quit)
-btn_cls.grid(column=3, row=5, pady=5)
+    ("Arial"), 10, 'bold'), bg='#1B5E20',width=12,height=1, fg='#FFFFFF', command=root.quit)
+btn_cls.grid(column=0, row=5, pady=5)
+
+# button Reverse Image
+btn_Reverse_Image = Button(root, text="Reverse", font=(
+    ("Arial"), 10, 'bold'), bg='#43A047',width=14,height=1,fg='#FFFFFF', command=lambda: Reverse_Image(temp_img))
+btn_Reverse_Image.grid(column=2, row=3, pady=5)
+
+# button Image Thresholding
+btn_Image_Thresholding = Button(root, text="Thresholding", font=(
+    ("Arial"), 10, 'bold'), bg='#43A047',width=14,height=1,fg='#FFFFFF' ,command=lambda: Image_Thresholding(temp_img))
+btn_Image_Thresholding.grid(column=2, row=4, pady=5)
+
+# button Laplacian
+btn_Laplacian = Button(root, text="Laplacian", font=(
+    ("Arial"), 10, 'bold'), bg='#43A047',width=14,height=1,fg='#FFFFFF' ,command=lambda: Laplacian(temp_img))
+btn_Laplacian.grid(column=2, row=5, pady=5)
+
+# button Gray Scale
+btn_Gray_Scale = Button(root, text="Gray Scale", font=(
+    ("Arial"), 10, 'bold'), bg='#43A047',width=14,height=1,fg='#FFFFFF', command=lambda: Gray_Scale(temp_img))
+btn_Gray_Scale.grid(column=3, row=3, pady=5)
+
+# button Histogram
+btn_Histogram = Button(root, text="Histogram", font=(
+    ("Arial"), 10, 'bold'), bg='#43A047',width=14,height=1,fg='#FFFFFF', command=lambda: Histogram(temp_img))
+btn_Histogram.grid(column=3, row=4, pady=5)
+
+# button Binary
+btn_Binary = Button(root, text="Binary", font=(
+    ("Arial"), 10, 'bold'), bg='#43A047',width=14,height=1,fg='#FFFFFF', command=lambda: Binary(temp_img))
+btn_Binary.grid(column=3, row=5, pady=5)
+
+# button Sobel
+btn_sobel = Button(root, text="Sobel", font=(
+    ("Arial"), 10, 'bold'), bg='#43A047',width=14,height=1, fg='#FFFFFF', command=lambda: Sobel(temp_img))
+btn_sobel.grid(column=4, row=3, pady=5)
+
+# button Logarithmic transforms
+btn_Log_transf = Button(root, text="Logarithmic T", font=(
+    ("Arial"), 10, 'bold'), bg='#43A047',width=14,height=1,fg='#FFFFFF', command=lambda: Log_transf(temp_img))
+btn_Log_transf.grid(column=4, row=4, pady=5)
+
+# button Grey level Slicing
+btn_GLS = Button(root, text="Grey LS", font=(
+    ("Arial"), 10, 'bold'), bg='#43A047',width=14,height=1,fg='#FFFFFF', command=lambda: GLS(temp_img))
+btn_GLS.grid(column=4, row=5, pady=5)
+
+# button Smoothing Image
+btn_Smoothing_Image = Button(root, text="Smoothing", font=(
+    ("Arial"), 10, 'bold'), bg='#43A047',width=14,height=1,fg='#FFFFFF', command=lambda: Smoothing_Image(temp_img))
+btn_Smoothing_Image.grid(column=5, row=3, pady=5)
+
+# button Laplace and gaussian
+btn_laplace_and_gau = Button(root, text="L & Gaussian", font=(
+    ("Arial"), 10, 'bold'), bg='#43A047',width=14,height=1,fg='#FFFFFF' ,command=lambda: laplace_and_gau(temp_img))
+btn_laplace_and_gau.grid(column=5, row=4, pady=5)
+
+
 
 root.mainloop()
